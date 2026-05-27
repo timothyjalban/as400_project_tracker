201e24c62aeef50e2e2e338409a05ea2c6d39f0a
Tim Alban <tim.alban@sanlorenzolumber.com>
2026-05-27 13:52:49 -0700

    Fix Milgard vendor SKU mapping

--- data/vendors.py
+++ data/vendors.py
@@ -1,5 +1,5 @@
 COMMON_VENDORS = [
-    {'name': 'Milgard', 'sku': 1001},
+    {'name': 'Milgard', 'sku': 660509},
     {'name': 'Andersen', 'sku': 1002},
     {'name': 'Pella', 'sku': 1003},
     {'name': 'Jeld-Wen', 'sku': 2001},
